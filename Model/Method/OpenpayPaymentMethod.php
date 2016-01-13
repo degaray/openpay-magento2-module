7a4e034218397ea78a6e76d286bc1da4a2df17ce
Xavier de <degaray@yahoo.com>
2016-01-13 11:11:37 -0600

    make payment method visble

--- Model/Method/OpenpayPaymentMethod.php
+++ Model/Method/OpenpayPaymentMethod.php
@@ -10,11 +10,18 @@ namespace Degaray\Openpay\Model\Method;
 
 
 use Magento\Payment\Model\Method\AbstractMethod;
-use Magento\Payment\Model\Method\Cc;
 
-class OpenpayPaymentMethod extends Cc
+class OpenpayPaymentMethod extends AbstractMethod
 {
     const METHOD_CODE = 'openpay';
+
+    protected $_code = self::METHOD_CODE;
+
+    protected $_isGateway                   = true;
+    protected $_canCapture                  = true;
+    protected $_canCapturePartial           = true;
+    protected $_canRefund                   = true;
+    protected $_canRefundInvoicePartial     = true;
 
     public function validate()
     {
